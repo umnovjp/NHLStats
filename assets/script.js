016139487fa6387aa1b5cf3d2afabce691160304
umnovjp <umnovjp@yahoo.com>
2024-08-13 23:47:58 -0500

    not a major update lack of time

--- assets/script.js
+++ assets/script.js
@@ -384,10 +384,9 @@ function getInputValue() {
               return response.json();
             })
             .then(function (data) {
-              // console.log(data.liveData.plays);
               const arrayShotsHome = [];
               const arrayShotsRoad = [];
-              const arrayShots = [];
+            //  const arrayShots = [];
 
               for (i = 0; i < data.plays.length; i++) {
                 if (data.plays[i].typeDescKey === 'shot-on-goal') {
@@ -408,56 +407,18 @@ function getInputValue() {
               var save = document.createElement('span');
               save.innerHTML = 'SV,';
               document.getElementById(savior).appendChild(save)
-                }
-                }
-//                   const onestring = data.liveData.plays.allPlays[i].players;
-//                   const one = JSON.stringify(onestring);        data.plays[i].typeDescKey==='goal'
-//                   testArray = one.split("fullName");
-//                   name1 = testArray[1].slice(3);
-//                   name1array = name1.split('"');
-//                   fullNameShooter = name1array[0];
-//                   name2 = testArray[2].slice(3);
-//                   name2array = name2.split('"');
-//                   fullNameSavior = name2array[0];
-//                     //         console.log(testArray, fullNameShooter, fullNameSavior);
-//                     //         console.log(data.liveData.plays.allPlays[i].coordinates);
-//                   var foWin = document.createElement('span');
-//                   var foLoss = document.createElement('span');
-//                   foWin.innerHTML = 'SH,';
-//                   foLoss.innerHTML = 'SV,';
-//                   var check1 = document.getElementById(fullNameShooter);
-//                   var check2 = document.getElementById(fullNameSavior);
-//                   if (check1 == null || check2 == null)
-//                   {console.log('error in shots', fullNameShooter)} //Daniel Sprong
-//                   else {
-//                   document.getElementById(fullNameShooter).appendChild(foWin);
-//                   document.getElementById(fullNameSavior).appendChild(foLoss);
-// console.log(fullNameShooter, fullNameSavior)}
-//                   var coordinates = { x: data.liveData.plays.allPlays[i].coordinates.x, y: data.liveData.plays.allPlays[i].coordinates.y };
-//                   arrayShots.push(coordinates);
-//                   if (document.getElementById('gameInfoAway').textContent.includes(fullNameShooter))
-//                   {arrayShotsRoad.push(coordinates)}
-                  
-//                    else if (document.getElementById('gameInfoHome').textContent.includes(fullNameShooter))
-//                    {arrayShotsHome.push(coordinates)} 
-//                    else console.log('error');
-                }
-              }
+                }}}}
               // console.log(arrayShots);
               new Chart("shotsChart", {
                 type: "scatter",
                 data: {
-                  datasets: [{
-                    pointRadius: 4,
+                  datasets: [{pointRadius: 4,
                     pointBackgroundColor: "rgb(0,0,255)",
-                    data: arrayShotsHome                    
-                  },
+                    data: arrayShotsHome},
                   {
                     pointRadius: 4,
                     pointBackgroundColor: "rgb(0,255,0)",
-                    data: arrayShotsRoad               
-                  }
-                ]
+                    data: arrayShotsRoad}]
                 },
                 options: {
                   legend: { display: true,
